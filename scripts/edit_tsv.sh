591e906e784182501aa93b50579cc645eb66ccc5
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2022-01-16 19:13:45 +0100

    Update edit_tsv

--- scripts/edit_tsv.sh
+++ scripts/edit_tsv.sh
@@ -1,1 +1,8 @@
-sed -i 's/|/_/g' ../HTSEQ/*.tsv
+#!/bin/bash
+for file in ../HTSEQ/*.tsv; do
+    outputfile="../counts/Filtered_"$(basename $file)
+    echo "filter "$file
+    # change from per exon to per gene count  
+    sed 's/|.*\t/\t/' $file \
+    | awk '{arr[$1]+=$2} END {for (key in arr) printf("%s\t%s\n", key, arr[key])}' | sort -k1,1 > $outputfile;
+    done
--- scripts/run.sh
+++ scripts/run.sh
@@ -22,7 +22,7 @@ for file in $rnaseq/*; do
     ./htseq.sh $filename $annot
     done
 
-# modify TSV by replacing | with _ (needed for R reports)
+#Change count per exon to count per gene
 ./edit_tsv.sh
 
 echo "Ending RNASeq Analysis Pipeline"
